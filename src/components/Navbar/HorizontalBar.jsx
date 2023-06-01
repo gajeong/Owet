2e314026309d27a39774ebbcee1e67eb796defb0
gajeong <rlarkwjd127@gmail.com>
2023-06-01 15:15:14 +0900

    horizontal bar ui

--- src/components/Navbar/HorizontalBar.jsx
+++ src/components/Navbar/HorizontalBar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import { HiBars3 } from 'react-icons/hi2'
+import { LuShoppingCart, LuSearch } from 'react-icons/lu'
+import { AiOutlineUser } from 'react-icons/ai'
+import { ReactComponent as Logo } from '../../assets/logo.svg'
+
 export default function HorizontalBar() {
   return (
-    <header>
-      <HiBars3 />
+    <header className='flex items-center justify-between p-4'>
+      <div className='w-28 flex-none'>
+        <HiBars3 className='cursor-pointer' />
+      </div>
+      <Logo width='64' className='cursor-pointer' />
+      <div className='flex flex-none grid-cols-3 gap-4 w-28 justify-end'>
+        <AiOutlineUser className='cursor-pointer' />
+        <LuShoppingCart className='cursor-pointer' />
+        <LuSearch className='cursor-pointer' />
+      </div>
     </header>
   )
 }
