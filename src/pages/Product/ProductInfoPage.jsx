5342f3c18e11fe05626e70575917cc8f15be0d08
gajeong <rlarkwjd127@gmail.com>
2023-06-09 18:28:03 +0900

    Mock up new product page

--- src/pages/Product/ProductInfoPage.jsx
+++ src/pages/Product/ProductInfoPage.jsx
@@ -1,19 +1,5 @@
 import React from 'react'
 
 export default function ProductInfoPage() {
-  return (
-    <>
-      <form>
-        <h2>신규 상품 등록</h2>
-        <img src='' alt='' />
-        <aside>
-          <input type='text' name='id' required />
-          <input type='text' name='itemNm' required />
-          <input type='text' name='detail' required />
-          <input type='text' name='price' required />
-          <input option name='sort' required />
-        </aside>
-      </form>
-    </>
-  )
+  return <>상품정보</>
 }
